e6045eb44ced1ce962f6215bc09637fb5c50bd11
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-09-02 14:34:19 +0700

    added files

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Arifin Firdaus on 02/09/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
