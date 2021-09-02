7951f517822b4909e624fe6f7471b2681f3b430d
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-09-02 15:24:06 +0700

    Demo `LinearGradient`

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LinearGradient(
+            gradient: Gradient(colors: [.white, .black]),
+            startPoint: .top,
+            endPoint: .bottom
+        )
     }
 }
 
